e795df5c4c4a8b49ec8666798a5ad466a62e8c4a
Sara LaFassett <sara@lafassett.com>
2013-09-19 15:30:16 -0700

    Added first two ruby apps to the repository

---
+++ alpha_sort.rb
@@ -0,0 +1,26 @@
+puts "What words would you like to sort?"
+puts "Type one word per line, pressing 'Enter' after each word."
+puts "After you have finished your list, press 'Enter'"
+
+words = []  #creates empty array
+new_word = gets.chomp  #prompts user for first word
+
+until new_word == ""  #as long as there is a new word, continue
+    words << new_word  #adds new_word to the array
+    new_word = gets.chomp  #prompts user for addiitional word
+end
+
+puts "Would you like to sort 'alpha' or 'reverse'?"
+sort_type = gets.chomp
+
+case sort_type
+    when "alpha" then words.sort!
+    when "reverse" then words.sort!.reverse!
+    else puts "Sorry, I don't know that sort order."
+        puts "Please choose 'alpha' or 'reverse'"
+        sort_type = gets.chomp
+end
+
+puts ""
+puts "Here is your #{sort_type} sorted list:"
+words.each{|x| puts x}
---
+++ bottles_of_beer.rb
@@ -0,0 +1,15 @@
+bottles = 99
+
+until bottles == 0
+    puts "#{bottles} bottles of beer on the wall!"
+    puts "#{bottles} bottles of beer!"
+    puts "You take one down, pass it around..."
+    bottles -= 1
+    puts "#{bottles} bottles of beer on the wall"
+    puts ""
+end
+
+if bottles == 0
+    puts "No more bottles of beer on the wall!"
+    puts ":-("
+end
